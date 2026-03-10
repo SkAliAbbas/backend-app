c8d401c0d082c9d719d5f9e81380475b221478ca
SkAliAbbas <shaikaliabbas10@gmail.com>
2026-03-10 13:42:20 +0530

    updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,5 +3,29 @@ const getproducts = async (req,res) => {
     const products = await productModel.find();
     res.render("products/index", { products }); 
 }
+const addProduct = async (req,res) => {
+    const product = req.body
+    await productModel.create(product);
+    res.redirect("/products")
+};
 
-export {getproducts};
+const addProductForm = async (req,res) => {
+    res.render("products/add");
+}
+const deleteProduct = async (req,res) => {
+    const id = req.params.id;
+    await productModel.findByIdAndDelete(id);
+    res.redirect("/products");
+}
+
+const editProductForm = async (req,res) => {
+    const id = req.params.id;
+    const product = await productModel.findOne({ _id: id });
+    res.render("products/edit", { product });
+}
+const saveProduct = async (req,res) => {
+    const id = req.params.id;
+    await productModel.findByIdAndUpdate(id, req.body);
+    res.redirect("/products");
+}
+export {getproducts,addProduct,addProductForm,deleteProduct,editProductForm,saveProduct};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,11 @@
-import { getproducts } from "../controllers/productController.js";
+import { getproducts,addProductForm, addProduct,deleteProduct,editProductForm,saveProduct } from "../controllers/productController.js";
 import express from "express";
 
 const productRouter = express.Router();
 productRouter.get("/", getproducts);
-
+productRouter.get("/add",addProductForm);
+productRouter.post("/add",addProduct);
+productRouter.get("/:id/delete",deleteProduct);
+productRouter.get("/:id/edit",editProductForm);
+productRouter.post("/:id/save",saveProduct);
 export {productRouter};
